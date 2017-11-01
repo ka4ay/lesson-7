2d5684a368143398aa5db7e79ab7030ac945286e
ka4ay <misha.ka4ay@yandex.ru>
2017-11-02 02:03:23 +0300

    new

--- barrels.py
+++ barrels.py
@@ -1,9 +1,14 @@
 import random
 
 barrels = [n for n in range(1, 91)]
-print(barrels)
+print(barrels, end='\n')
 for x in range(90, 0, -1):
     i = random.choice(barrels)
     barrels.remove(i)
 
-    print(i, x)
+    for r in range(1, 4):
+        for c in range(1, 10):
+            print(i, end='\t')
+        print('\n')
+
+    #print(i, x, end='\t')
